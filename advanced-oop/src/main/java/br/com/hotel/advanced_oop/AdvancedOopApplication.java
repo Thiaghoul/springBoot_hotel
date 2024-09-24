a99ceb3464b7a5de2d8926b9f8a8a8b32029eb3a
Thiago Nunes <thiagohnunes.w@gmail.com>
2024-09-24 01:36:08 -0300

    First commit

---
+++ advanced-oop/src/main/java/br/com/hotel/advanced_oop/AdvancedOopApplication.java
@@ -0,0 +1,14 @@
+package br.com.hotel.advanced_oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AdvancedOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedOopApplication.class, args);
+	}
+
+}
---
+++ advanced-oop/src/test/java/br/com/hotel/advanced_oop/AdvancedOopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hotel.advanced_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
